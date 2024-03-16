c4e7e0a225138112338b4cf3c409f68dd2c1a00a
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-16 14:20:48 -0400

    intial commit of node.js

---
+++ greatesvalue.js
@@ -0,0 +1,13 @@
+var read = require("readline-sync")
+var num1  = read.question("enter a number")
+var num2 = read.question("enter the second number")
+if(num1 > num2 ){
+console.log("num1 is greatest")
+
+}
+else if(num2>num1){
+    console.log("num2 is the greatest")
+}
+else{
+    console.log("both are equal")
+}
---
+++ hello.js
@@ -0,0 +1,4 @@
+var input= require("readline-sync")
+var name=input.question("enter a name")
+console.log("name "+name)
+
