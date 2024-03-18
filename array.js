4be4bc06ebdd4add02510f6b3b738a8a43c507ee
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-18 16:37:13 -0400

    scope  and local

---
+++ array.js
@@ -0,0 +1,7 @@
+var array=["sajith","arjun","abi","hello"]
+console.log(array)
+console.log(array.length)
+for (var i=0;i<4;i++){
+    for(var j=0;j<4;j++){
+    console.log(array[j])}
+}
---
+++ nested.js
@@ -0,0 +1,16 @@
+var a =10
+function hello(){
+    var a=20
+    var a=20
+ 
+    function hey(){
+        
+        console.log(a)
+
+    }
+    hey()
+
+}
+hello()
+console.log(a)
+
