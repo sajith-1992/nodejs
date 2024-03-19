42ece8a224872222c18df3943385e8ed0d32feeb
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-19 10:15:56 -0400

    date object constructor

--- objectconstructor.js
+++ objectconstructor.js
@@ -14,6 +14,9 @@ var hoi  =  new person("abi","kannur","still finding")
 hello.display=function(){
     console.log("hai this is from display function"+this.name)
 }
+var timeIn= new Date("10/12/2024")
+console.log(timeIn.getFullYear())
 
-hello.display()
+
+
 
