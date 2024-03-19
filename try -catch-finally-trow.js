7ea8bf71aa127d1a427eba9eb58f6d8952b0ccea
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-19 11:40:33 -0400

    try catch finally and trow in js

--- try -catch-finally-trow.js
+++ try -catch-finally-trow.js
@@ -1,5 +1,23 @@
+var read=require("readline-sync")
+var take= read.question("enter a value").jkas
 try{
+    
+    if (take>5){
+        console.log("try is working")
+    
+    
+    }
+    else{
+        throw"this is lessthan 5"
+    }
+}
 
-}catch{
+catch (error){
+    console.log (error)
+}
+    
 
-}fin
+finally{
+
+    console.log("i need to work always")
+}
