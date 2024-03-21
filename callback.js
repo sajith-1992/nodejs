68b1a33af9531e61330de196a3faec6ef9e22e9d
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-21 16:45:24 -0400

    date difference

---
+++ callback.js
@@ -0,0 +1,7 @@
+var dt=new Date()
+for(var i=0;i<1000;i++){
+    console.log("loop")
+
+}
+ var result= new Date()-dt
+ console.log(result)
--- es6 arrowfunction.js
+++ es6 arrowfunction.js
@@ -6,3 +6,7 @@ let cat=(num1,num2)=> {
     return(num1+num2)
 }
 console.log(cat(10,20))
+hello=(num1,num2)=>{
+    console.log(num1+num2)
+}
+hello(10,20)
