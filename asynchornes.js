90da63e146836fe7baa295456ebd2e7ce2c48287
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-21 18:04:36 -0400

    set time out for asychornes funtion

---
+++ asynchornes.js
@@ -0,0 +1,15 @@
+
+show=()=>{
+    console.log("end")
+}
+console.log("start")
+
+setTimeout(show,3000)
+
+console.log("start")
+
+setTimeout(show,3000)
+
+console.log("start")
+
+setTimeout(show,3000)
