1d086bcb381031a4ccaca04311a69371991dc109
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-25 09:58:56 -0400

    url prase and req

--- create_server.js
+++ create_server.js
@@ -1,9 +1,12 @@
 var http = require('http');
 var fs = require('fs');
+var url=require("url")
+
 
 http.createServer(function (req, res) {
-
-    if(req.url==="/")
+    var q =url.parse(req.url)
+    console.log(q.pathname)
+    if(q.pathname==="/")
     {fs.readFile('sample.html', 'utf8', function(err, html) {
         if (err) {
             // Handle error
@@ -16,16 +19,51 @@ http.createServer(function (req, res) {
             res.end(); //end the response
         }
     })}
-    else if(req.url==="/admin"){
+    else if(q.pathname==="/admin"){
         res.write("this is admin")
         res.end()
 
-    }else if(req.url==="/user"){
+    }else if(q.pathname==="/user"){
         res.write("this is user page ")
         res.end()
 
     }
-    ;
+    else if(q.pathname==="/signup")
+    {fs.readFile('signup.html', 'utf8', function(err, html) {
+        if (err) {
+            // Handle error
+            console.error('Error reading HTML file:', err);
+            res.writeHead(500, {'Content-Type': 'text/plain'});
+            res.end('Internal Server Error');
+        } else {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(html); //write the HTML content to the response
+            res.end(); //end the response
+        }
+    })
+
+    } else if(q.pathname==="/signupaction"){
+        fs.readFile('signupform.html',(err,data)=>{
+            if (err) {
+                // Handle error
+                console.error('Error reading HTML file:', err);
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.end('Internal Server Error');
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data); //write the HTML content to the response
+                res.end(); //end the response
+            }
+        })
+        
+    }
+
+
+    
+        
+
+  
+
 }).listen(3000, function() {
     console.log('Server is running on port 3000');
 });
