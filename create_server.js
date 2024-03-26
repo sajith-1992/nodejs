cdbd3bfc30ef49d8dfde638baf4f4f583399f99e
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-26 17:48:29 -0400

    to console,the form data in form of object

--- create_server.js
+++ create_server.js
@@ -4,7 +4,7 @@ var url=require("url")
 
 
 http.createServer(function (req, res) {
-    var q =url.parse(req.url)
+    var q =url.parse(req.url,true)
     console.log(q.pathname)
     if(q.pathname==="/")
     {fs.readFile('sample.html', 'utf8', function(err, html) {
@@ -45,7 +45,7 @@ http.createServer(function (req, res) {
 
     } else if(q.pathname==="/signupaction"){ 
         
-        
+        console.log(q.query)
         fs.readFile('signupform.html',(err,data)=>{
             if (err) {
                 // Handle error
