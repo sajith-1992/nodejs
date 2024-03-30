8b16c02f76cb938fd14ba39dc9a7e1294fb050dc
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-30 17:55:23 -0400

    send data from form to another html page and printing it on the page

--- express.js
+++ express.js
@@ -1,6 +1,9 @@
  const express=require("express")
  const path = require("path")
  const app = express()
+ const bodyParser = require('body-parser');
+ app.use(bodyParser.urlencoded({ extended: true}))
+ app.use(express.static('public'));
 
 app.use(function(req,res,next){
     console.log("working")
@@ -10,11 +13,28 @@ app.use(function(req,res,next){
 
 
  app.get('/sign',(req,res)=>{
+
     res.sendFile (path.join(__dirname,"/signup.html"))
  })
-app.post('/sign',(req,res)=>{
-    res.send("hai")
-})
+ 
+ app.get('/signupfinish',(req,res)=>{
+   const emailup = req.query.email;
+   console.log(emailup)
+    res.sendFile (path.join(__dirname,"/signupfinish.html",))
+ })
+
+
+
+
+
+ app.post('/sign', (req, res) => {
+    console.log(req.body);
+    var email = req.body.email;
+   
+    res.redirect(`/signupfinish?email=${encodeURIComponent(email)}`);
+   });
+
+
  app.listen(3000,()=>{
  console.log(__dirname)
  console.log(__filename)
